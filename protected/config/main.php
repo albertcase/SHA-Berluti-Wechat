953d8e4be9a47ac6b1b292f1ce6143706e24a852
Demon Zhang <demon.zhang@samesamechina.com>
2015-10-14 11:33:13 +0800

    no message

--- protected/config/main.php
+++ protected/config/main.php
@@ -74,7 +74,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=urbandecay',
+			'connectionString' => 'mysql:host=localhost;dbname=berluti',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '1qazxsw2',
