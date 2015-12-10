a8fb75194a6450eb27b1d20e53af885b60ddaf73
Vincen Wen <vincen.wen@samesamechina.com>
2015-12-10 11:55:25 +0800

    no message

--- customize/js/loading.js
+++ customize/js/loading.js
@@ -1,50 +1,50 @@
 var LoadingImg = [
-	"../img/arr.png",
-	"../img/backArr.png",
-	"../img/contactText.png",
-	"../img/email.png",
-	"../img/hands.png",
-	"../img/indexBg.jpg",
-	"../img/insideBg.jpg",
-	"../img/logo_black.png",
-	"../img/logo.png",
-	"../img/mask.jpg",
-	"../img/name.png",
+	"img/arr.png",
+	"img/backArr.png",
+	"img/contactText.png",
+	"img/email.png",
+	"img/hands.png",
+	"img/indexBg.jpg",
+	"img/insideBg.jpg",
+	"img/logo_black.png",
+	"img/logo.png",
+	"img/mask.jpg",
+	"img/name.png",
 
-	"../img/orderBtn.jpg",
-	"../img/p1.png",
-	"../img/p2.png",
-	"../img/p3.png",
-	"../img/p4.png",
-	"../img/p5.png",
-	"../img/p6.png",
+	"img/orderBtn.jpg",
+	"img/p1.png",
+	"img/p2.png",
+	"img/p3.png",
+	"img/p4.png",
+	"img/p5.png",
+	"img/p6.png",
 
-	"../img/p7.png",
-	"../img/play.png",
-	"../img/poster.jpg",
-	"../img/shoeTree.png",
-	"../img/shoseBg.png",
-	"../img/slogan.png",
-	"../img/startBtn.png",
+	"img/p7.png",
+	"img/play.png",
+	"img/poster.jpg",
+	"img/shoeTree.png",
+	"img/shoseBg.png",
+	"img/slogan.png",
+	"img/startBtn.png",
 
-	"../img/tel.png",
-	"../img/tmTips.png",
+	"img/tel.png",
+	"img/tmTips.png",
 
-	"../img/pro/pro1_draw.png",
-	"../img/pro/pro2_draw.png",
-	"../img/pro/pro3_draw.png",
-	"../img/pro/pro4_draw.png",
-	"../img/pro/pro5_draw.png",
-	"../img/pro/pro6_draw.png",
-	"../img/pro/pro7_draw.png",
+	"img/pro/pro1_draw.png",
+	"img/pro/pro2_draw.png",
+	"img/pro/pro3_draw.png",
+	"img/pro/pro4_draw.png",
+	"img/pro/pro5_draw.png",
+	//"img/pro/pro6_draw.png",
+	//"img/pro/pro7_draw.png",
 
-	"../img/pro/pro1.png",
-	"../img/pro/pro2.png",
-	"../img/pro/pro3.png",
-	"../img/pro/pro4.png",
-	"../img/pro/pro5.png",
-	"../img/pro/pro6.png",
-	"../img/pro/pro7.png"
+	"img/pro/pro1.png",
+	"img/pro/pro2.png",
+	"img/pro/pro3.png",
+	"img/pro/pro4.png",
+	"img/pro/pro5.png",
+	//"img/pro/pro6.png",
+	"img/pro/pro7.png"
 
 ]
 
@@ -76,10 +76,12 @@ function loading(allAmg){
 		$("img").each(function(){ 
 			$(this).attr("src",$(this).attr("sourcesrc"));
 		})
-
+		$(".loading").hide();
+		
 	} , function (p){
+		$("#dreambox").stop().animate({"opacity": p});
 		//$("#loading span").html("<i>"+p+"%</i>");
-		//console.log(p);
+		console.log(p);
 	});
 }
 
