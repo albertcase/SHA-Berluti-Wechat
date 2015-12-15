93940ff2a83447bd3bee23dea93e575510569a10
Vincen Wen <vincen.wen@samesamechina.com>
2015-12-15 11:57:58 +0800

    no message

--- customize/js/public.js
+++ customize/js/public.js
@@ -1,13 +1,8 @@
-if(!!self.touch) self.slider.addEventListener('touchstart',self.events,false); 
 
-//定义touchstart的事件处理函数
-function start(event){
-　　var touch = event.targetTouches[0]; //touches数组对象获得屏幕上所有的touch，取第一个touch
-　　startPos = {x:touch.pageX,y:touch.pageY,time:+new Date}; //取第一个touch的坐标值
-　　isScrolling = 0; //这个参数判断是垂直滚动还是水平滚动
-　　this.slider.addEventListener('touchmove',this,false);
-　　this.slider.addEventListener('touchend',this,false);
-}
+$(".stop-swiping").bind("touchstart",function (event) {
+    event.preventDefault();
+})
+
 
 /* 微信分享 */
 
