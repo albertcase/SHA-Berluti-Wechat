ee9140d601d29e6991231b8c0fa7dcb2863027cb
Vincen Wen <vincen.wen@samesamechina.com>
2016-06-27 17:21:16 +0800

    no message

---
+++ protected/controllers/ShoeController.php
@@ -0,0 +1,15 @@
+<?php
+
+class ShoeController extends Controller
+{
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+}
