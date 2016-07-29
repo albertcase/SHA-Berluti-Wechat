c681a2a423141c4b4ae7a5d7f9292ccfa4ed7187
Vincen's <vincen.wen@samesamechina.com>
2016-07-29 11:00:17 +0800

    no message

--- protected/views/fasttrack/koldetailed.php
+++ protected/views/fasttrack/koldetailed.php
@@ -31,7 +31,15 @@
 
 						<div class="con">
 							<img src="/vstyle/fasttrack/img/pro/m0/v1.jpg" width="100%">
-							<p>董荷斌，毕业于中国政法大学国际经济法专业，大学主修法律，辅修计算机，现任美力三生科技有限公司(iLife3)市场传播与公关副总裁。他曾是奥美公司晋升最快的中国籍员工，其学识不下于刘同，犀利不输于马丁。因参加大型求职类综艺节目《职来职往》而被观众所熟知，还是北京音乐广播台的每周五《男左女右》节目主持人。</p>
+							<p>当人们还在为《速度与激情》里的酷炫漂移感到热血沸腾时，董荷斌，这个精通6门外语，驱车驾驭375公里时速的龙的传人，已经成为了赛车界的黑马，把电影里风驰电掣的特技变为了现实。 这个魅力型男不仅在跑道上有着异于常人的坚韧和果敢，举手投足间也凸显出一股独特的迷人气质，在时尚的跑道上勇往直前。</p>
+							<p>董荷斌第一次和卡丁车邂逅是在14岁，那时他就和赛车结下了不解之缘。对速度的渴望，和15年不改初心的坚持，促使他永远要求自己把事情做到极致。赛车是世界上唯一不能练习的运动，因为车手的比赛用车，很少能作为其日常的练习车使用。所以车手唯一能做的就是提升体能以应对赛场上的各种速度和挑战。运动生活一定少不了合适的鞋履来应对重重挑战。董荷斌对选择鞋履极富美学追求，不仅要在外观上符合他始终如一的真我个性，还要在设计上满足人性化的健康需求。</p>
+							<p>Berluti延逾百年匠心工艺，打造了凝聚速度与激情的Fast Track运动鞋。鞋面选用最优质的釉面小牛皮或植鞣小牛皮制作，柔软轻盈，舒适透气；传统的染色技法让皮革散发出迷人质感；而防滑的胶底在步履间增添些许动感气息，将追风掣电的速度和韵律展现得淋漓尽致。</p>
+							<p>许多人认为冲破终点的瞬间最热血，但董荷斌却喜欢在每一段赛道中享受超越极限带来的快感。正是这种从一而终的追求，才能和Berluti Fast Track的超越创新融为一体，在长途奔袭的赛道上延续运动风尚。</p>
+
+							<p>When people are excited about the drifting special effect in Fast & Furious, Dong Hebin, a Chinese boy who speaks 6 languages and can control a car at the speed of 375km/h, has already realized those exciting moments in the real world by becoming a racing star. The sunny boy shows his perseverance and talent on the track, and he also keeps a sense of taste in account of his behavior, which makes him go straightly on the fashion road.</p>
+							<p>For Dong Hebin, he was destined to be a racer when he drove a kart at the age of 14. Dong Hebin’s energy comes from the desire for speed. Sticking to his dream for 15 years, he came to know that when a person tries his best to do one thing, accumulates piece by piece, and improves step by step, he will finally make a different, just like a carbon turning to a diamond. Racing is the only sport that cannot get trained, because the racing cars are rarely used for daily practice. That’s why the only thing racers can do is to build up their health, and develop their physical ability to adjust the speed and challenges. So fashion to Dong Hebin is not just a beautiful and healthy equipment, but a thing that can keep its quality forever.</p>
+							<p>Berluti, with hundred-year craftsmanship, brings Fast Track Sport Shoes with extraordinary comfort and elegance. It is made from patina-look calfskin or glazed calf, and the traditional patina craft equips the shoes with an attractive texture, at the same time the non-slip sole shares the similar beauty with the sport spirit of the shoes. So the pair of shoes is so magic that can attract the owner to a fashion sport world.</p>
+							<p>Many people think it is the most exciting moment to pass the destination, but Dong Hebin prefers to enjoy in each track. Thanks to this consistent pursuit to the dream, it combines with the transcendent and innovative spirit of Berluti Fast Track, and preserves the trendy sport feel on the fashion road as long as possible.</p>
 							<center>拍摄花絮</center>
 							 <div class="videoArea">
 								<div id="vplay"></div>
@@ -55,7 +63,16 @@
 
 						<div class="con">
 							<img src="/vstyle/fasttrack/img/pro/m1/v0.jpg" width="100%">
-							<p>陈默，毕业于中国政法大学国际经济法专业，大学主修法律，辅修计算机，现任美力三生科技有限公司(iLife3)市场传播与公关副总裁。他曾是奥美公司晋升最快的中国籍员工，其学识不下于刘同，犀利不输于马丁。因参加大型求职类综艺节目《职来职往》而被观众所熟知，还是北京音乐广播台的每周五《男左女右》节目主持人。</p>
+							<p>陈默，公关专家，时尚型男，镜头前阳光帅气、渊博犀利，荧幕后德才兼备，机智过人。如今陈默再创职场传奇，却依旧朝气蓬勃，努力拼搏。可是抛开成功的光环，不是所有人都了解他领先时代、永不落后的秘密。</p>
+							<p>懂得如何选择、在选择后拼尽全力，这就是陈默摆脱无趣生活的信条。总是奔走在台前幕后的他，常常一人分饰多角：面对创业之路的风雨，陈默总是坚毅冷静;但就算再忙再累，也不忘分出时间和朋友相聚，共享难得的友情时光。</p>
+							<p>对于陈默而言，鞋履是与身体融为一体的，它承受了生命之重，也支撑起了人的精神和情怀。Berluti Fast Track，拥有无可挑剔的植鞣小牛皮，由天然及矿物性鞣革制成，搭配精美得体的动感设计和经典雅致的染色技艺，不仅带来活力夺目的独特外形，还能塑造优雅庄重的经典气质，行走之间带来愉悦的舒适体验。穿上这双最佳伴履 — 既能奔走生意职场，又能亲赴友人派对, 享受无拘无束，完美多面的人生！ </p>
+							<p>世间一切经典都是在磨砺中蜕变，又在蜕变中坚守它独有的本源。Berluti的品牌精神如此，陈默的为人处世亦如此。正是这种对生活和工作不改初心的极致要求，才不断推进一个人的成长和收获。正如陈默所言：“时光会雕琢你，也会雕琢那些你曾经觉得难以面对的坎坷。”经时间淬炼出的品质，因风雨洗礼后的风姿，显现出更加绚烂的光辉，历久弥新，恒久传承。</p>
+
+							<p>Chen Mo, once an experienced public relation expert, is now a leader who is fashionable, handsome, knowledgeable, and eloquent. Having an extraordinary performance, Chen Mo made a great contribution in any workplace he worked. He now has started a greater entrepreneurship, and works hard with a group of friends who share the same dreams to create a quality life. Regardless his success, do you really know the secret of Chen Mo’s trendy idea?</p>
+							<p>Make choice carefully and then fight for your choice – this is Chen Mo’s principle to make a different. As the leader of public relation department, he has to be tough and responsible, and always plays different role in the daily life. On the road to entrepreneurship, not only does he have to endure loneliness, but also he needs to stay firm and calm to break through all the difficulties. However, although Chen Mo is strict with his job, he still keeps in touch with his friends and cheers them up.</p>
+							<p>To Chen Mo, a product needs to take care of fashion and health. A pair of shoes is not for covering, but something that helps you establish your confidence and spirit – not only does it entail artistic beauties, but also it takes care of the owner’s health by offering great comforts, as if it becomes parts of your body. </p>
+							<p>The Berluti Fast Track Sport Shoes is made from fine quality Patina-look calfskin, which is a natural and mineral tanning. In addition, the shoes are designed with dynamic beauty, classic patina, and unforgettable experience – all of these features exactly meet the demands of Chen Mo. Wearing these personally designed shoes, it seems that you are walking to youth and quality life, which can be fit in business occasion or casual friend parties. No matter how different your life is, Berluti’s fashion product can always meet your needs. Can anyone say no to these shoes?</p>
+							<p>A person’s growth is related to his the hardships he has been through, while he also must maintain some valuable qualities during the process. Due to these unchangeable qualities, Chen Mo can improve himself step by step and try to balance his life and health. This is also part of the brand spirit of Berluti, just as Chen Mo says: “You had a tough time in the past, but those miserable moments also made your life colorful.” The quality that can endure time, can finally turn to classics and last forever. Let’s see how the legend last on August 14th. </p>
 						</div>
 						<span class="arr"></span>
 					</div>
@@ -72,8 +89,16 @@
 							</div>
 						</div>
 						<div class="con">
-							<img src="/vstyle/fasttrack/img/pro/m2/v2.jpg" width="100%">
-							<p>王博文，毕业于中国政法大学国际经济法专业，大学主修法律，辅修计算机，现任美力三生科技有限公司(iLife3)市场传播与公关副总裁。他曾是奥美公司晋升最快的中国籍员工，其学识不下于刘同，犀利不输于马丁。因参加大型求职类综艺节目《职来职往》而被观众所熟知，还是北京音乐广播台的每周五《男左女右》节目主持人。</p>
+							<img src="/vstyle/fasttrack/img/pro/m2/v1.jpg" width="100%">
+							<p>男士的魅力随历练的累积而散发，从13年的乒乓训练与3年的星途，我们看到的不仅是他如何以球技征服对手, 以歌声打动观众，更是如何从国家一级运动员成功蜕变为现在这位敢于突破自我且善于展现个人独特风格的全能艺人。他，就是魅力风发的王博文。</p>
+							<p>君士不器，男士都必须为自己走出一条专属的道路。2013年，这位青春大男孩王博文毅然放弃了熟悉的乒乓生涯，转战到截然不同的音乐世界。3年后，他以充满热诚与希望的音符向我们证明了只要有梦想，没有不能实现的可能。这份超人的勇气, 你也有吗？</p>
+							<p>想要摆脱无趣的生活，就必须创新突破。这是王博文的时尚觉悟，亦是Berluti Fast Track运动鞋的设计初衷。Fast Track分为植鞣小牛皮和釉面小牛皮两种：植鞣小牛皮的鞋款分为蓝色和棕色两种款式，鞋头印有对称的点面花纹，鞋面上的古法染色修饰得恰到好处，鞋舌则回归庄严的牛津鞋设计，既有英伦的高贵，又有运动的活泼；釉面小牛皮的鞋款则在鞋舌上采用了交叉鞋带的创意，凭添青春趣味。</p>
+							<p>王博文用自己的梦想诠释了青春的意义，将Fast Track的率真和无畏，载入步履之间。无论是成长挑战，还是拼搏狂欢，Berluti始终如影随形，助你加速前行。</p>
+
+							<p>The glamour of men is accumulated from life experience. According to his 13 years table tennis training and 3 years singing history, not only do we see a tough player who conquered his competitor, but also we witness a music lover transforms from the first-class athlete to a great artist who dares to challenge himself. He is Wang Bowen, an attractive young singer.</p>
+							<p>A great man may never be restricted by any forms or traditions but to live a life as his wishes. Wang Bowen gave up playing table tennis in 2013 and got involved in the entertainment industry. After 3 years, he became a better man: every melody in his song entailed his passion and dreams. And his story just proved that nothing was impossible as long as you tried. Do you have the same courage like him?</p>
+							<p>The future is full of uncertainties, but you need to innovate to get what you want. This is Wang Bowen’s fashion concept, and it’s also Berluti’s designing insight to create Fast Track. The elegant color is one of the biggest features of Fast Track. According to the material, Fast Track can be divided into patina-look calfskin and glazed calf. The patina-look calfskin is known for the patina craftsmanship, which has brown and black types of shoes, and there are holes patterns at the shoe top. It combines Oxford shoes’ formality and sports shoes’ casualness. The glazed skin type has crossed lace at the tongue, and the black color brings a sense of energy to the shoes. All of them are very comfortable and seem to say: “Step to dignity; step with confidence!”</p>
+							<p>Wang Bowen redefines the meaning of youth with him dream, and he combines the truthfulness and braveness of Fast Track with his own character when he wears the shoes. No matter he takes adventures or enjoys the excitement of life, Berluti will always be with him like a best friend. </p>
 						</div>
 						<span class="arr"></span>
 					</div>
