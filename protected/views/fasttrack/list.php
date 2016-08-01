a5182ab2a8429d6087848f1f6444f6c8a0458bfc
Vincen's <vincen.wen@samesamechina.com>
2016-08-01 11:28:24 +0800

    no message

--- protected/views/fasttrack/list.php
+++ protected/views/fasttrack/list.php
@@ -96,7 +96,7 @@
 			$(".voteList li").eq(2).find("a img").attr("src", "/vstyle/fasttrack/img/avater-"+voteNum[2]["_id"]+".png");
 
 
-			$("#voteResult").fadeIn();
+			$("#voteResult").show();
 		});
 
 </script>
