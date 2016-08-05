16045479d4125704c29783dc361bdc0e88ebcc1d
Vincen's <vincen.wen@samesamechina.com>
2016-08-05 16:15:46 +0800

    no message

--- protected/views/fasttrack/video.php
+++ protected/views/fasttrack/video.php
@@ -29,7 +29,16 @@
 			<!-- Swiper -->
 		    <div class="swiper-container" id="videoSwiper">
 		        <div class="swiper-wrapper">
-					
+					<div class="swiper-slide">
+						<div class="videoModel">
+							<div class="con ycenter">
+								<a href="/fasttrack/videoDetailed" class="playIcon"></a>
+								<img src="/vstyle/fasttrack/img/place.jpg" width="100%">
+							</div>
+							<img src="/vstyle/fasttrack/img/v_bg.png" width="100%">
+						</div>
+		            </div>
+		            
 					<?php if($version >= "2") {
 					?>
 						<div class="swiper-slide">
@@ -43,16 +52,6 @@
 			            </div>
 			        <?php
 					}?>
-					
-		            <div class="swiper-slide">
-						<div class="videoModel">
-							<div class="con ycenter">
-								<a href="/fasttrack/videoDetailed" class="playIcon"></a>
-								<img src="/vstyle/fasttrack/img/place.jpg" width="100%">
-							</div>
-							<img src="/vstyle/fasttrack/img/v_bg.png" width="100%">
-						</div>
-		            </div>
 
 		            <div class="swiper-slide">
 						<div class="videoModel">
